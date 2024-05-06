a3fa6b252f358feacaa574797ce82956d7706960
ashok parihar <aparihar@email.arizona.edu>
2024-05-06 00:15:51 -0600

    Making corrections to the imports in the models folder.

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,6 @@
 // Imports
 const { Schema, model } = require("mongoose");
-const reactionSchema = require("../Reaction");
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
   {
