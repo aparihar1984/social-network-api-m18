0ada6ff05ec1473b04f054c504856666d501a61e
ashok parihar <aparihar@email.arizona.edu>
2024-05-05 21:17:19 -0600

    Adding our models based off the assignment criteria.

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+// Imports
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("../Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Created a virtual called reactionCount that retrieves the length of the thoughts reactions array field on query.
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+// Exports
+module.exports = Thought;
