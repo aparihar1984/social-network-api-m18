0c5cbd43082df86e50ae58834cfd7e9a3d6dea67
ashok parihar <aparihar@email.arizona.edu>
2024-05-05 19:57:58 -0600

    Adding the server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+// Imports
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Server is listening for requests and sending a response
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
